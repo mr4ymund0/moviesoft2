333b2ad077f5ea1663722cb765541a3d04c98fed
mr4ymund0 <mr4ymund0@gmail.com>
2018-05-31 12:26:31 -0500

    add function delete

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ database = []
 @app.route('/movies', methods = ['POST', 'GET'])
 def index():
 	if request.method == 'POST':
+		id = len(database) + 1
 		name = request.form['name']
 		year = request.form['year']
 		director = request.form['director']
-		movie = {'name': name, 'year': year, 'director': director}
+		movie = {'id':id,'name': name, 'year': year, 'director': director}
 		database.append(movie)
 		return jsonify(movie)
+	return jsonify(database)
+
+@app.route('/movies/<int:id>', methods = ['GET'])
+def search(id):
+	mov_filter = list(filter(lambda n: n['id'] == id, database))
+	return jsonify(mov_filter)
+
+@app.route('/movies/<int:id>', methods = ['DELETE'])
+def delete(id):
+	mov_delete = list(filter(lambda n: n['id'] == id, database))
+	database.remove(mov_delete[0])
 	return jsonify(database)
 
 
